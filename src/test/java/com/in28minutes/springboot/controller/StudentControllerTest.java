b00ce9a9823cbf946de6869a332f9eaa6c620c64
mjkumar-rgda <54305140+mjkumar-rgda@users.noreply.github.com>
2020-09-21 16:53:31 +0530

    corrected test cases

--- src/test/java/com/in28minutes/springboot/controller/StudentControllerTest.java
+++ src/test/java/com/in28minutes/springboot/controller/StudentControllerTest.java
@@ -54,7 +54,7 @@ public class StudentControllerTest {
 		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
 		System.out.println("Response   : " + result.getResponse().getContentAsString());
-		String expected = "{\"id\":\"Course1111\",\"name\":\"Spring\",\"description\":\"10 Steps\",\"steps\":[\"Learn Maven\",\"Import Project\",\"First Example\",\"Second Example\"]}";
+		String expected = "{\"id\":\"Course1\",\"name\":\"Spring\",\"description\":\"10 Steps\",\"steps\":[\"Learn Maven\",\"Import Project\",\"First Example\",\"Second Example\"]}";
 
 		// {"id":"Course1","name":"Spring","description":"10 Steps, 25 Examples and 10K Students","steps":["Learn Maven","Import Project","First Example","Second Example"]}
 
