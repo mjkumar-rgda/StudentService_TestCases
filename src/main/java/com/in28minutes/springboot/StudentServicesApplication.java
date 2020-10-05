597bd272bc6bf8975a2b67fd105ebc3bb569bafa
mjkumar-rgda <54305140+mjkumar-rgda@users.noreply.github.com>
2020-10-05 16:54:01 +0530

    Update StudentServicesApplication.java

--- src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
+++ src/main/java/com/in28minutes/springboot/StudentServicesApplication.java
@@ -7,8 +7,15 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 //@PropertySource(value = {"file:StudentService_TestCases/application.properties"})
 public class StudentServicesApplication {
+	
+	private static final Logger LOGGER = LogManager.getLogger(StudentServicesApplication.class);
+	
+	@Value("${spring.datasource.driver-class-name}")
+	private String driver;
 
 	public static void main(String[] args) {
+		
+		LOGGER.info(" Driver details   : "+ driver);
 		SpringApplication.run(StudentServicesApplication.class, args);
 		
 	}
